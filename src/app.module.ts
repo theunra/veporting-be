540b56232c7f962d0a346f045f5d9e66a44cf592
theunra <diandrarizqy7@gmail.com>
2024-02-21 13:42:59 +0700

    access database using typeormmodule and split entities to separate route/api endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportModule } from './report/report.module';
+import { ConfigModule } from '@nestjs/config';
+import { ModelModule } from './model/model.module';
 
 @Module({
-  imports: [ReportModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true,}),
+    ModelModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,25 +1,89 @@
 import { Injectable } from '@nestjs/common';
-import * as db from 'src/model/db';
+import { InjectRepository } from '@nestjs/typeorm';
+import {Report} from 'src/report/entities/Report';
+import { EntityManager, Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+export enum ProductType {
+    PENETRATION = 'penetration',
+    VULNERABILITY = 'vulnerability',
+}
+
+export enum TestMethod {
+    GREYBOX = 'greybox',
+}
+
+function productTypeIdx(product_type: string){
+    const vals = Object.values(ProductType);
+    return vals.findIndex((type)=>type==product_type);
+}
+
+function testMethodIdx(test_method: string){
+    const vals = Object.values(TestMethod);
+    return vals.findIndex((type)=>type==test_method);
+}
 
 @Injectable()
 export class ReportService {
+    constructor(
+        @InjectRepository(Report)
+        private reportRepository : Repository<Report>,
+        private readonly entityManager : EntityManager,
+    ){}
+
     async getAllReport(){
-        return db.getAllReport();
+        return this.reportRepository.find({});
     }
 
     async createReport(report){        
-        return db.createReport(report);
+        const product_type = productTypeIdx(report.product_type);
+        const test_method = testMethodIdx(report.test_method);
+
+        if(product_type < 0) return;
+        if(test_method < 0) return;
+
+        const new_report = this.reportRepository.create({
+            id: uuidv4(),
+            client_name : report.client_name,
+            test_method : test_method,
+            product_type : product_type,
+            report_date : new Date(report.report_date),
+            end_date : new Date(report.end_date),
+            created_at : new Date(),
+            updated_at : new Date()
+        });
+
+        return this.reportRepository.save(new_report);
     }
 
     async getReport(id: string){
-        return db.getReportById(id);
+        return this.reportRepository.findOne({
+            where:{
+                id: id,
+            }
+        });
     }
 
     async updateReport(id: string, reportUpdate){
-        return db.updateReportById(id, reportUpdate);
+        const report = await this.getReport(id);
+
+        if(!report) return;
+
+        if(reportUpdate.product_type){
+            reportUpdate.product_type = productTypeIdx(reportUpdate.product_type);
+        }
+
+        const updated_report = {
+            ...report,
+            ...reportUpdate,
+            updated_at : new Date(),
+        }
+
+        return this.reportRepository.save(updated_report);
     }
 
     async deleteReport(id: string){
-        return db.deleteReportById(id);
+        const report = await this.getReport(id);
+        return this.reportRepository.remove(report);
     }
 }
