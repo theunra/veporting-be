efd9de820389c08251e19054402bedcbe6b668dd
Rizki Firmansyah <rizkifirmansyah2642@gmail.com>
2024-03-06 18:17:55 +0700

    fix email

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,41 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ReportService } from './report.service';
 import { CreateReportDto, UpdateReportDto } from 'src/report/dto/report.dto';
 
 @Controller('report')
 export class ReportController {
-    constructor (private readonly reportService: ReportService){};
+  constructor(private readonly reportService: ReportService) {}
 
-    //TESTING
-    @Get('all')
-    async getAllReport(){
-        return await this.reportService.getAllReport();
-    }
+  //TESTING
+  @Get('all')
+  async getAllReport() {
+    return await this.reportService.getAllReport();
+  }
 
-    @Get(':id')
-    async getReport(
-        @Param('id') id: string,
-    ){
-        return await this.reportService.getReport(id);
-    }
+  @Get(':id')
+  async getReport(@Param('id') id: string) {
+    return await this.reportService.getReport(id);
+  }
 
-    @Post('create')
-    async createReport(@Body() report : CreateReportDto,){
-        return await this.reportService.createReport(report);
-    }
+  @Post('create')
+  async createReport(@Body() report: CreateReportDto) {
+    return await this.reportService.createReport(report);
+  }
 
-    @Put(':id')
-    async updateReportById(
-        @Param('id') id:string,
-        @Body() reportUpdate : UpdateReportDto,
-    ){
-        return await this.reportService.updateReport(id, reportUpdate);
-    }
+  @Put(':id')
+  async updateReportById(
+    @Param('id') id: string,
+    @Body() reportUpdate: UpdateReportDto,
+  ) {
+    return await this.reportService.updateReport(id, reportUpdate);
+  }
 
-    @Delete(':id')
-    async deleteReportById(
-        @Param('id') id:string,
-    ){
-        return await this.reportService.deleteReport(id);
-    }
+  @Delete(':id')
+  async deleteReportById(@Param('id') id: string) {
+    return await this.reportService.deleteReport(id);
+  }
 }
